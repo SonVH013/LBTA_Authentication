978b4d283e4c9ac0eff064d59260fa2bfc0b1765
SonVH <sonvh013@gmail.com>
2019-04-02 17:49:04 +0700

    - success get token when login fb

--- AuthenticationApp/ViewController.swift
+++ AuthenticationApp/ViewController.swift
@@ -11,7 +11,18 @@ import FBSDKLoginKit
 
 class ViewController: UIViewController, FBSDKLoginButtonDelegate {
     func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
-        print("login complete")
+        if error != nil {
+            print(error)
+            return
+        }
+        
+        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"])?.start(completionHandler: { (connection, result, error) in
+            if error != nil {
+                print("Fail to graph request: \(error)")
+                return
+            }
+            print(result)
+        })
     }
     
     func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
@@ -26,10 +37,24 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         view.addSubview(loginButton)
         //frame's are obselete, please use constraints instead because its 2016 after all
         loginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
-        
         loginButton.delegate = self
+        
+        //add button
+        let customBtn = UIButton(frame: CGRect(x: 16, y: 150, width: view.frame.width - 32, height: 50))
+        customBtn.setTitle("Custom Login Facebook", for: .normal)
+        customBtn.backgroundColor = UIColor.blue
+        customBtn.addTarget(self, action: #selector(btnLoginTouched), for: UIControl.Event.touchUpInside)
+        view.addSubview(customBtn)
     }
 
-
+    @objc func btnLoginTouched() {
+        print("1234")
+        FBSDKLoginManager().logIn(withReadPermissions: ["email", "public_profile"], from: self) { (result, error) in
+            if error != nil {
+                print("error login wth permission: \(error)")
+            }
+            print(result?.token.tokenString)
+        }
+    }
 }
 
